3ea90acaaa31a82d30463f421075951ee1c99e6d
KyykMaks <ripperkyyk@gmail.com>
2023-11-11 19:59:34 +0200

    Task 4

--- lesson-04/js/module_6.js
+++ lesson-04/js/module_6.js
@@ -5,27 +5,22 @@ const text = `Об'єктна модель документа (Document Object M
 
 const words = text.split(/\s+/); // Розбиваємо текст на слова за допомогою роздільників (пробіли, коми, крапки і т.д.)
 
-
 const pEl = document.createElement("p");
 
-words.forEach(word => {
-    if(word.length >= 8){
-        const spanEl = document.createElement("span");
-        spanEl.style.backgroundColor = "yellow";
-        spanEl.textContent = word+" ";
-        pEl.appendChild(spanEl)
-    } else {
-        pEl.innerHTML += word+" ";
-    }  
-
-})
+words.forEach((word) => {
+  if (word.length >= 8) {
+    const spanEl = document.createElement("span");
+    spanEl.style.backgroundColor = "yellow";
+    spanEl.textContent = word + " ";
+    pEl.appendChild(spanEl);
+  } else {
+    pEl.innerHTML += word + " ";
+  }
+});
 
 const linkEl = document.querySelector("a");
 
 linkEl.before(pEl);
-
-
-
 
 //TODO:=========task-03=================
 // Створіть контейнер div (з класом numberContainer )в HTML-документі та динамічно створіть 100 блоків (з класом number) наповнивши їх рандомними числами від 1 до 100 і додайте їх до контейнера div(numberContainer). Парні числа повинні мати зелений фон (додати клас even), Непарні числа - жовтий фон (додати клас odd).
@@ -37,24 +32,21 @@ const divCon = document.createElement("div");
 divCon.classList.add("number-container");
 const divArr = [];
 
-for (let i = 0; i < BLOCKS; i ++){
-    const div = document.createElement("div");
-    div.classList.add("number");
-    div.textContent = randomNumber();
+for (let i = 0; i < BLOCKS; i++) {
+  const div = document.createElement("div");
+  div.classList.add("number");
+  div.textContent = randomNumber();
 
-    if (div.textContent % 2 === 0){
-        div.classList.add("even");
-    }else{
-        div.classList.add("odd");
-    }
-    divArr.push(div);
+  if (div.textContent % 2 === 0) {
+    div.classList.add("even");
+  } else {
+    div.classList.add("odd");
+  }
+  divArr.push(div);
 }
 
 divCon.append(...divArr);
 linkEl.after(divCon);
-
-
-
 
 //TODO:=========task-04=================
 /**
@@ -73,13 +65,94 @@ linkEl.after(divCon);
 
 // find refs
 
-//TODO:===========1===============
+// const inputEl = document.querySelector(".js-username-input");
+// const formEl = document.querySelector(".js-contact-form");
+// const spanEl = document.querySelector(".js-username-output");
+// //TODO:===========1===============
 
-//TODO:===========2===============
+// // inputEl.addEventListener('input', handleInput )
 
-//TODO:===========3===============
+// // function handleInput (event){
 
-//TODO:===========4===============
+// // const input = event.target;
+
+// // if(input.value.trim().length > 6 ){
+// //     input.classList.add('success');
+// //     input.classList.remove('error');
+// // } else {
+// //     input.classList.remove('success');
+// //     input.classList.add('error');
+// // }
+
+// // }
+
+// //TODO:===========2===============
+
+// // inputEl.addEventListener("focus", (event) => {
+// //   const input = event.target;
+// //   !input.value.trim()
+// //     ? (input.style.outline = "3px solid red")
+// //     : (input.style.outline = "3px solid green");
+// // });
+
+// //TODO:===========3===============
+
+
+// // inputEl.addEventListener("blur", (event) => {
+// //       const input = event.target;
+// //       !input.value.trim()
+// //         ? (input.style.outline = "3px solid red")
+// //         : (input.style.outline = "3px solid lime");
+// //     });
+
+
+// //TODO:===========4===============
+
+// formEl.addEventListener("submit", (event) => {
+  
+//     event.preventDefault();
+
+// let formData = new FormData(event.target);
+
+// // console.log(formData);
+
+// const userName = formData.get('userName');
+// const acceptEl = formData.get ('accept');
+
+// if ( !userName || !acceptEl ){
+
+//         return alert ("Заповніть поля")
+//     }
+    
+// console.log({userName:userName});
+
+// // formData = new FormData();
+// event.target.elements.userName.value = '';
+// event.target.elements.accept.checked = false;
+
+// spanEl.textContent = "Anonymous";
+
+// console.log(userName);
+
+// const {accept, userName} = event.target.elements;
+
+
+// if ( !userName.value.trim() || !accept.checked ){
+//     return alert ("Заповніть поля")
+// }
+// console.log({userName:userName.value});
+
+// event.target.reset(); 
+// spanEl.textContent = "Anonymous";
+//  });
+
+
+// inputEl.addEventListener('input', (event) => {
+//  spanEl.textContent = event.target.value.trim() || 'Anonymous' ;
+// })
+
+
+
 
 //TODO:=========task-05=================
 // Кнопка "Зменшити" робить квадрат меншим на 20 пікселів, кнопка "Збільшити" - робить більшим на 10 пікселів.
