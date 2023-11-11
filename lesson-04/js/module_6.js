d8aa77945e417f8c40890ba3fdc6e335325544cb
jaycikey <138725941+jaycikey@users.noreply.github.com>
2023-11-11 11:53:59 -0500

    taskTwo

--- lesson-04/js/module_6.js
+++ lesson-04/js/module_6.js
@@ -30,8 +30,31 @@ linkEl.before(pEl);
 //TODO:=========task-03=================
 // Створіть контейнер div (з класом numberContainer )в HTML-документі та динамічно створіть 100 блоків (з класом number) наповнивши їх рандомними числами від 1 до 100 і додайте їх до контейнера div(numberContainer). Парні числа повинні мати зелений фон (додати клас even), Непарні числа - жовтий фон (додати клас odd).
 
-// const randomNumber = () => Math.floor(Math.random() * 100) + 1;
-// const BLOCKS = 100;
+const randomNumber = () => Math.floor(Math.random() * 100) + 1;
+const BLOCKS = 100;
+
+const divCon = document.createElement("div");
+divCon.classList.add("number-container");
+const divArr = [];
+
+for (let i = 0; i < BLOCKS; i ++){
+    const div = document.createElement("div");
+    div.classList.add("number");
+    div.textContent = randomNumber();
+
+    if (div.textContent % 2 === 0){
+        div.classList.add("even");
+    }else{
+        div.classList.add("odd");
+    }
+    divArr.push(div);
+}
+
+divCon.append(...divArr);
+linkEl.after(divCon);
+
+
+
 
 //TODO:=========task-04=================
 /**
